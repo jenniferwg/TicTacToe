b52d5716323c840395649c8dbeeefa36251591a4
Jennifer Wang <jw775@cornell.edu>
2017-08-07 15:32:56 -0700

    add turn functionality

--- client/index.js
+++ client/index.js
@@ -1,7 +1,40 @@
-let boardState = [
+let initialBoard = [
   0, 0, 0,
   0, 0, 0,
   0, 0, 0
-]
+];
+let initialTurn = 'X';
+let currentBoard = initialBoard;
+let currentTurn = 'X';
 
-console.log($('body'));
+checkWin = function() {
+  return false;
+};
+
+newGame = function() {
+  currentBoard = initialBoard;
+  initialTurn = initialTurn === 'X' ? 'O' : 'X';
+  currentTurn = initialTurn;
+  for (let i = 0; i < $('td').length; i++) {
+    $($('td')[i]).text('');
+  }
+  $('.status').text(`Player ${currentTurn}'s turn!`);
+};
+
+$('button').on('click', function(e) {
+  newGame();
+});
+
+$('td').on('click', function(e) {
+  $(e.target).text(currentTurn);
+  const row = $(e.target).data('row');
+  const col = $(e.target).data('col');
+  currentBoard[row, col] = currentTurn;
+
+  if (checkWin()) {
+    $('.status').text(`Player ${currentTurn} wins!`);
+  } else {
+    currentTurn = currentTurn === 'X' ? 'O' : 'X';
+    $('.status').text(`Player ${currentTurn}'s turn!`);
+  }
+});
