2ed9a660a0f5f8e9cca0403301a4c074857ae6fe
Jennifer Wang <jw775@cornell.edu>
2017-08-07 15:50:30 -0700

    add win checks

--- client/index.js
+++ client/index.js
@@ -1,14 +1,46 @@
 let initialBoard = [
-  0, 0, 0,
-  0, 0, 0,
-  0, 0, 0
+  [ 0, 0, 0 ],
+  [ 0, 0, 0 ],
+  [ 0, 0, 0 ]
 ];
 let initialTurn = 'X';
 let currentBoard = initialBoard;
 let currentTurn = 'X';
 
 checkWin = function() {
-  return false;
+  let won = false;
+
+  for (let i = 0; i < 3; i++) {
+    //horizontal win
+    const currentRow = currentBoard[i].reduce((accum, cur) => accum + cur, '');
+
+    //vertical win
+    const cols = [];
+    currentBoard.forEach(col => { cols.push(col[i]); });
+    const currentCol = cols.reduce((accum, cur) => accum + cur, '')
+
+    if (currentTurn.repeat(3) === currentRow || currentTurn.repeat(3) === currentCol) {
+      won = true; 
+    }
+  }
+
+  if (won === false) {
+    //diagonal win
+    let col = 0;
+    let downDiag = currentBoard.reduce((accum, row) => {
+      col++;
+      return accum + row[col - 1];
+    }, '');
+    let upDiag = currentBoard.reduce((accum, row) => {
+      col--;
+      return accum + row[col];
+    }, '');
+    if (currentTurn.repeat(3) === upDiag || currentTurn.repeat(3) === downDiag) {
+      won = true; 
+    }
+  }
+
+  return won;
 };
 
 newGame = function() {
@@ -21,6 +53,7 @@ newGame = function() {
   $('.status').text(`Player ${currentTurn}'s turn!`);
 };
 
+//event handlers
 $('button').on('click', function(e) {
   newGame();
 });
@@ -29,7 +62,7 @@ $('td').on('click', function(e) {
   $(e.target).text(currentTurn);
   const row = $(e.target).data('row');
   const col = $(e.target).data('col');
-  currentBoard[row, col] = currentTurn;
+  currentBoard[row][col] = currentTurn;
 
   if (checkWin()) {
     $('.status').text(`Player ${currentTurn} wins!`);
